de405f1dc0a3c53fc40b55e36beb3fc18fc1c6ac
ddeveloper72 <duncan.niamh@gmail.com>
2024-07-16 00:07:16 +0100

    chore: Update views.py to include home and error handler functions

--- assistant/views.py
+++ assistant/views.py
@@ -1,3 +1,29 @@
+"""This module contains the views for the assistant app."""
 from django.shortcuts import render
+from openai import OpenAIError
+from .engine import chatgpt
 
-# Create your views here.
+
+def home(request):
+    """This function renders the home page of the assistant app."""
+    try:
+
+        if request.method == 'POST':
+            chats = chatgpt(request)
+            context = {
+                'prompt': chats['prompt'],
+                'response': chats['response']
+            }
+           
+            return render(request, 'assistant/home.html', context)
+        return render(request, 'assistant/home.html' )
+    
+    except OpenAIError as e:
+        # This block of code handles OpenAI API errors.
+        error_message = f"OpenAI API Error: {e}"
+        return render(request, 'assistant/error.html', {'error_message': error_message})
+    
+
+def error_handler(request):
+    """This function renders the error page of the assistant app."""
+    return render(request, 'assistant/error.html')
