876b79063b84895dd7f9c4904ccd54c02641d484
ddeveloper72 <duncan.niamh@gmail.com>
2024-07-28 14:22:38 +0100

    Update URL path for home page in assistant/urls.py

--- assistant/urls.py
+++ assistant/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('assistant/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('error', views.error_handler, name='error_handler'),
 ]
