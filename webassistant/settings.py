b25a39a5edb26565cab08a7942bcd54640c99482
ddeveloper72 <duncan.niamh@gmail.com>
2024-08-10 23:31:23 +0100

    chore: Update MEDIA_ROOT and STATICFILES_DIRS in settings.py
    Update the MEDIA_ROOT and STATICFILES_DIRS paths in settings.py to use the os.path.join() function for better cross-platform compatibility. This change ensures that the media and static files are correctly located and served by the application.
    
    Note: This commit message follows the established commit message conventions in the repository.

--- webassistant/settings.py
+++ webassistant/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,14 +133,18 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [BASE_DIR, 'media']
+MEDIA_ROOT = [
+    os.path.join(BASE_DIR, 'media')
+    ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
